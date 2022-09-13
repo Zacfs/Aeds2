538ecaff811530e7291890b5e6c38ec18a11b81e
Thiago Augusto Demeto Leão <PUCMINAS\1277657@srvcrcshell.pucminas.net>
2022-09-13 15:28:31 -0300

    Second commit

---
+++ hall.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+import java.nio.*;
+public class hall{
+	String[] murderers;
+	String[] victims;
+	public static void main(String[] args)
+	{	
+		System.out.println("HALL OF MURDERERS");	
+		String x;
+		int cont = 0;
+		String[] murd = new String[100];
+                String[] vic = new String[100];
+		Scanner a = new Scanner(System.in);
+			while(a.hasNext())
+			{
+			murd[cont] = a.next();
+			if(!(murd[cont].charAt(0)=='F' && murd[cont].charAt(1) == 'I' && murd[cont].charAt(2) == 'M'))
+			{
+			vic[cont] = a.next();
+			cont++;
+			}
+			}
+			int cont2 = cont;
+			for(int i = 0;i<cont;i++)
+			{
+				for(int v = 0;v<cont;v++)
+				{
+					if(murd[i].charAt(0) == vic[v].charAt(0) &&murd[i].charAt(1) == vic[v].charAt(1) && murd[i].charAt(2) == vic[v].charAt(2) )
+					{murd[i] = "null";
+					cont2--;}
+				}	
+			}
+					int cont3 = 0;
+				String[] murd2 = new String[cont2];
+				for(int i = 0;i<cont2;i++)
+				{
+					while(cont3<cont2)
+					{
+					if(murd[i] != "null")
+					{
+						murd2[cont3] = murd[i];
+						cont3++;
+						System.out.println(murd2[cont3]);
+					}
+					}
+				}
+				int[] mor = new int[cont2];
+				for(int i = 0; i<cont2;i++)
+				{mor[i] = 1;}
+                                                for(int y = 0; y<cont2; y++)
+                                                {
+                                                	for(int v = 0;v<cont2;v++)
+							{
+							if(murd2[y].charAt(0) == murd2[v].charAt(0) && murd2[y].charAt(1) == murd2[v].charAt(1) && murd2[y].charAt(2) == murd2[v].charAt(2))
+							{
+                                                        mor[y] = mor[y]++;
+							}
+							}
+                                                }
+					for(int i = 0;i<cont2;i++)
+					{
+						System.out.println(murd2[i] + " " + mor[i]);
+					}
+			a.close();
+
+	}}
